ed5d1b2d5f4ef17a4af42c253d28eda92c8ea1ff
Rizo I <rizo@odis.io>
2022-08-07 14:05:13 +0100

    Fix core cmd deprecation issue.

--- src/Cmd_core_bench.ml
+++ src/Cmd_core_bench.ml
@@ -1,8 +1,12 @@
 open Core_bench
-
 open Streams_bench.Cases
 
 let () =
+  main_tests ();
   Streams_bench.Config.configure ();
-  let tests = List.map (fun (name, f) -> Bench.Test.create ~name (Sys.opaque_identity f)) current in
-  Core.Command.run (Bench.make_command tests)
+  let tests =
+    List.map
+      (fun (name, f) -> Bench.Test.create ~name (Sys.opaque_identity f))
+      current
+  in
+  Command_unix.run (Bench.make_command tests)
